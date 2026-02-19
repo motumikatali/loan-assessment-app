9d0921f4550ce540c6be69820c5ba88160ac93a9
Motumi Katali <motumikatali@gmail.com>
2026-02-19 10:29:07 +0200

    Fix Render deployment

--- app.py
+++ app.py
@@ -28,4 +28,10 @@ def home():
 
     return render_template("index.html", result=result)
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 app.run()
